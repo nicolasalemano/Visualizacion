5ebfbcf7f7c5815852ed348fd4345ffc30e670a7
Nicolas <nico.alemano@gmail.com>
2018-11-06 23:28:26 -0300

    tp4

---
+++ tp4/js/main.js
@@ -0,0 +1,8 @@
+let juego = new Juego();
+
+
+document.getElementById('inicio').style.visibility='visible';
+
+document.getElementById("inicio").addEventListener("click", function () {
+    juego.comenzarJuego();
+});
