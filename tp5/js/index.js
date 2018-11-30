69e4968de914f39a89d9d92d3e835a7cc9b8fec2
Nicolas <nico.alemano@gmail.com>
2018-11-30 11:12:30 -0300

    car

--- tp5/js/index.js
+++ tp5/js/index.js
@@ -1,22 +1,14 @@
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-	alert("aca")
-  interval: false
+
+/*
+$(document).ready(function (){
+    evento.preventDefault();
+    
+alert("SS");
+login();
+
+
+
+
 });
 
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  console.log("aca");
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
+*/
